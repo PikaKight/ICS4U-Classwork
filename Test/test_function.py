0c5bb619088815207b63cbb9b033c7d5ade65620
Marcus <marcus.tuenmuk20@ycdsbk12.ca>
2019-10-02 13:01:42 -0400

    Update

--- Test/test_function.py
+++ Test/test_function.py
@@ -4,3 +4,7 @@ def test_sum():
     assert sum([1, 1, 1]) == 3
     assert sum([1] * 10000) == 10000
     assert sum([9]) == 9
+
+def test_f_strings():
+    a = 5
+    assert f"hello {a}" == "hello 5"
