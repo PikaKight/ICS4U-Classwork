68b0b05d10293186402719c50113017122202f00
Marcus <marcus.tuenmuk20@ycdsbk12.ca>
2019-10-03 14:25:50 -0400

    STUFF

--- Coding Practice/evaluation_prep.py
+++ Coding Practice/evaluation_prep.py
@@ -1,23 +1,25 @@
 from typing import *
 import math
 
+
 def sum_of_two(a: int, b: int) -> Int:
     """ Function that takes two integers and returns the sum 
-    
+
     Arguments:
-        a is the first integer
-        b is the second integer
+        a: the first integer
+        b: the second integer
     Return:
         sum of a and b  
 
     """
     return (a+b)
 
+
 def sum_of_list(List: list) -> Int:
     """ Returns the sum of a list of numbers
     Args:
-        List is the list of numbers
-        Total is the sum of
+        List: the list of numbers
+    Return: 
     """
     total = 0
     for i in List:
