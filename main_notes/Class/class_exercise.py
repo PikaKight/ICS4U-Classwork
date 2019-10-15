edb532b5468b305baa5be85dadf33a859567db85
Marcus <marcus.tuenmuk20@ycdsbk12.ca>
2019-10-15 13:05:28 -0400

    Updata

--- main_notes/Class/class_exercise.py
+++ main_notes/Class/class_exercise.py
@@ -29,17 +29,28 @@ class Person():
     def introduce(self):
         return (f"Hello, my name is {self.name}")
     
-    def punch(self, Person):
-        Person.health_points -= 10
+    def punch(self, Person, times):
+        if Person == self:
+            Person.health_points -= ((10 * 0.25) * times)
+        else:
+            Person.health_points -= (10 * times)
         print(f"{Person.name}'s health is {Person.health_points}")
 
+    def eat(self):
+        self.health_points = 100
+        print (f"{self.name}'s health is back to {self.health_points}")
 pikakight = Person("PikaKight", 161, 1)
 mars = Person("Mars", 161, 1)
 
 print(pikakight)
 print(mars)
 
-pikakight.punch(mars)
-mars.punch(pikakight)
-pikakight.punch(pikakight)
-mars.punch(mars)
+
+pikakight.punch(mars, 1)
+mars.punch(pikakight, 1)
+pikakight.punch(pikakight, 10)
+mars.punch(mars, 10)
+
+pikakight.eat()
+mars.eat()
+
