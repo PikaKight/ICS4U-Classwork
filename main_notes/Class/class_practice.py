479c6954505354cb83be9ed7535c41f6f8763129
Marcus <marcus.tuenmuk20@ycdsbk12.ca>
2019-10-15 12:24:33 -0400

    classes

--- main_notes/Class/class_practice.py
+++ main_notes/Class/class_practice.py
@@ -1,21 +1,33 @@
-Person
-======
-name: str
-height: int
-strength: int
-health_points: int = 100
----------
-__str__(self) -> str
-introduce(self) -> void
-punch(Person) -> void
-    
 class Person():
+    pass
+
+class Chair():
     """
     Attrs:
-        height (int): in cm
-        name (str): First and last
-        strength (int): How strong the person is 
-        health_points (int): Out of 100 (everyone starts at 100)
+        colour: str
+        legs: int
+        dimensions: list
+        materials: list
+        max_weight: int
+        occupant: Object
     """
-    def __init__(self, ...):
-        pass
+
+    def __init__(self, colour: str, legs: int, dimensions: list, materials: list, max_weight: int):
+        self.colour = colour
+        self.legs = legs
+        self.dimensions = dimensions
+        self.materials = materials
+        self.max_weight = max_weight
+        self.occupant: None
+
+c1 = Chair("Blue", 4, [100, 50], ["Plastic", "Metal"], 200)
+c2 = Chair("Green", 4,[100, 75], ["Plastic", "Metal"], 200)
+jeff = Person()
+
+c1.occupant = c2
+c2.occupant = jeff
+
+print(c1.colour, c1.legs, c1.dimensions, c1.materials, c1.max_weight, c1.occupant)
+
+print(c1.occupant.colour, c1.occupant.legs, c1.occupant.dimensions, c1.occupant.materials, c1.occupant.max_weight, c1.occupant.occupant)
+
