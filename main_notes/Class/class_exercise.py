6f56e5346f967b930cf94c8b2a772649195c0e19
Marcus <marcus.tuenmuk20@ycdsbk12.ca>
2019-10-15 14:25:59 -0400

    Updata

--- main_notes/Class/class_exercise.py
+++ main_notes/Class/class_exercise.py
@@ -13,10 +13,11 @@ punch(Person) -> void
 
 class Person():
 
-    def __init__(self, name: str, height: int, strength: int):
+    def __init__(self, name: str, height: int, strength: int, defense: int):
         self.name = name
         self.height = height
         self.strength = strength
+        self.defense = defense
         self.health_points = 100
         
     def __str__(self):
@@ -31,16 +32,19 @@ class Person():
     
     def punch(self, Person, times):
         if Person == self:
-            Person.health_points -= ((10 * 0.25) * times)
+            Person.health_points -= ((10 * 0.25) * times)* ((self.strength - Person.defense) / 100)
+
         else:
-            Person.health_points -= (10 * times)
+            Person.health_points -= ((10 * times) * ((self.strength - Person.defense) / 100))
+
         print(f"{Person.name}'s health is {Person.health_points}")
 
     def eat(self):
         self.health_points = 100
         print (f"{self.name}'s health is back to {self.health_points}")
-pikakight = Person("PikaKight", 161, 1)
-mars = Person("Mars", 161, 1)
+
+pikakight = Person("PikaKight", 161, 1, 100)
+mars = Person("Mars", 161, 1, 50)
 
 print(pikakight)
 print(mars)
