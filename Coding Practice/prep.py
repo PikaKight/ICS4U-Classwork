a38edccd6d51ba16a128c65285ec1ba69d474083
Marcus <marcus.tuenmuk20@ycdsbk12.ca>
2019-10-18 13:18:01 -0400

    update

---
+++ Coding Practice/prep.py
@@ -0,0 +1,31 @@
+from typing import *
+class Food():
+    
+    def __init__(self, name: str, cost: int, nutrition: int):
+        self.name = name
+        self.cost = cost
+        self.nutrition = nutrition
+
+class Dog():
+
+    def __init__(self, breed: str, name: str, happiness: int, food):
+        self.breed = breed
+        self.name = name
+        self.happiness = happiness
+        self.eat(food)
+        self.bark()
+
+    def __str__(self) -> str:
+        return(f"{self.name}: {self.happiness}")
+
+    def eat(self,Food):
+        self.happiness += (0.10 * Food.nutrition)
+
+    def bark(self):
+        return("RUFF RUFF!")
+
+food = Food("apple", 1, 50)
+
+dog = Dog("Husky", "Fenrir", 100, food)
+
+print(dog, dog.bark())
