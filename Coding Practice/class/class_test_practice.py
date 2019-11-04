6860a06ae1b0871371e2d77d0296c54524f77968
Marcus <marcus.tuenmuk20@ycdsbk12.ca>
2019-11-04 13:34:57 -0500

    update

---
+++ Coding Practice/class/class_test_practice.py
@@ -0,0 +1,7 @@
+class Person:
+
+    def __int__(self, first_name: str, last_name: str, email: str):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        
