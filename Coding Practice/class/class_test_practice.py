b4931c7b64d02351e8ebf9122171d47623a6bae2
Marcus <marcus.tuenmuk20@ycdsbk12.ca>
2019-11-04 14:07:10 -0500

    stuff

--- Coding Practice/class/class_test_practice.py
+++ Coding Practice/class/class_test_practice.py
@@ -1,7 +1,20 @@
+from typing import *
+from datetime import date
 class Person:
+    def __init__(self, first_name: str, last_name: str, Dob: date):
+        self._first_name = first_name
+        self._last_name = last_name
+        self._email = None
+        self._dob = Dob
 
-    def __int__(self, first_name: str, last_name: str, email: str):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        
+    def greet(self) -> str:
+        return f"Hello, I am {self._first_name} {self._last_name}."
+
+    def get_age(self) -> int:
+        today = date.today()
+        self._age = today.year - self._dob.year - ((today.month, today.day) < (self._dob.month, self._dob.day))
+        return self._age
+
+p = Person("Marcus", "Tuen Muk", date(2002, 10, 19))
+print(p.get_age())
+
